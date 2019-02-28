64821cb2cb62e5d47dbf86497d06c3ff40956983
Justin Lange <j.lange@gmx.com>
2019-02-28 11:02:43 -0400

    Added Header Comments

--- app/src/main/java/com/example/roshambo/MainActivity.java
+++ app/src/main/java/com/example/roshambo/MainActivity.java
@@ -1,3 +1,14 @@
+/*
+    File:  MainActivity.java
+
+    Author: Justin Lange
+    This assignment represents my own work and is in accordance with the College Academic Policy
+
+    Copyright(c) 2019 All Right Reserved by Justin Lange
+
+    Date: 28-02-19
+*/
+
 package com.example.roshambo;
 
 import android.support.v7.app.AppCompatActivity;
